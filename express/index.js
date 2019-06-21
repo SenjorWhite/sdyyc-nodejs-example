75b24aed85e058dcba719c2d3846085f8b49a118
senjorwhite <senjorwhite@gmail.com>
2019-06-21 13:39:46 -0600

    Added: /api/members in index.js.

--- express/index.js
+++ express/index.js
@@ -2,6 +2,22 @@ const express = require("express");
 const app = express();
 const port = 3900;
 
+const members = [
+    { id: "001", name: "Senjor" },
+    { id: "002", name: "Alex" },
+    { id: "003", name: "Tony" },
+    { id: "004", name: "John" },
+    { id: "005", name: "Twilight" },
+]
+
+app.get("/api/members", (req, res) => {
+    res.send(members);
+});
+
+app.get("/", (req, res) => {
+    res.send({ Error: "Api not found." });
+});
+
 app.listen(port, () => {
     console.log("Express server is listening on Port: %s", port);
 });
